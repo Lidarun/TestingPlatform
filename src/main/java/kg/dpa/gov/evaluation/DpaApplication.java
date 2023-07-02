bb78aeafa68df8eaa677fcc32f726c7ab19d85da
General Admiral Nuradil <a.nuradil.m@gmail.com>
2023-07-02 19:45:43 +0600

    add: localization configuration for yaml file, keys storage in *yaml file

--- src/main/java/kg/dpa/gov/evaluation/DpaApplication.java
+++ src/main/java/kg/dpa/gov/evaluation/DpaApplication.java
@@ -2,7 +2,9 @@ package kg.dpa.gov.evaluation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class DpaApplication {
 
--- src/main/java/kg/dpa/gov/evaluation/controllers/LanguageController.java
+++ src/main/java/kg/dpa/gov/evaluation/controllers/LanguageController.java
@@ -5,6 +5,8 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 import java.util.Locale;
 
@@ -12,11 +14,14 @@ import java.util.Locale;
 public class LanguageController {
 
 //    @GetMapping("/changeLanguage")
-//    public String changeLanguage(@RequestParam("lang") String lang,
+//    public String changeLanguage(@RequestParam("lang") String language,
 //                                 HttpServletRequest request,
 //                                 HttpServletResponse response) {
-//        Locale locale = new Locale(lang);
-//        request.getSession().setAttribute("locale", locale);
-//        return "redirect:/";
+//        Locale locale = new Locale(language);
+//        LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+//        localeResolver.setLocale(request, response, locale);
+//
+//        String referer = request.getHeader("Referer");
+//        return "redirect:" + (referer != null ? referer : "/");
 //    }
 }
